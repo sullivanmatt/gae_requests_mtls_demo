8b16c0b0c596135c43763a8faec64cc00bde197f
Matthew Sullivan <sullivan.matt@gmail.com>
2015-07-23 12:18:58 -0500

    Add is_called_by_requests function

--- lib/httplib.py
+++ lib/httplib.py
@@ -7,6 +7,13 @@ from python_std_lib import httplib as sockets_httplib
 # every variable.
 globals().update(gae_httplib.__dict__)
 
+def is_called_by_requests():
+	# Get the module that called us two operations up the stack
+    caller = inspect.stack()[2]
+    module = inspect.getmodule(caller[0]).__name__
+    return module.startswith('requests.packages.urllib3.')
 
-class HTTPConnection(sockets_httplib.HTTPConnection if inspect.getmodule(inspect.stack()[1][0]).__name__.startswith('requests.packages.urllib3.') else gae_httplib.HTTPConnection, object):
+# If is_called_by_requests(), HTTPConnection should subclass python_std_lib.httplib.HTTPConnection,
+# else HTTPConnection will subclass gae_override.httplib.HTTPConnection
+class HTTPConnection(sockets_httplib.HTTPConnection if is_called_by_requests() else gae_httplib.HTTPConnection, object):
     pass
